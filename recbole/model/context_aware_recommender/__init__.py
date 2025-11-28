99f0ef357baebe655ad043a2613746aecdc231cf
Emre Atilgan <emreatilgancfl@gmail.com>
2025-11-28 14:42:21 +0100

    feat(model): add CageCTR context-aware recommendation model

--- recbole/model/context_aware_recommender/__init__.py
+++ recbole/model/context_aware_recommender/__init__.py
@@ -17,4 +17,4 @@ from recbole.model.context_aware_recommender.fignn import FiGNN
 from recbole.model.context_aware_recommender.kd_dagfm import KD_DAGFM
 from recbole.model.context_aware_recommender.eulernet import EulerNet
 
-from recbole.model.context_aware_recommender.cage_ctr import CageCTR
+from recbole.model.context_aware_recommender.cagectr import CageCTR
